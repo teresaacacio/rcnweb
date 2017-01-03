e01379298fea3a4944435a71d4eaeaf5f72eb6e0
Teresa Acacio <teresa@realcognita.com>
2017-01-03 10:31:37 +0800

    Plain RCN page

--- index.php
+++ index.php
@@ -3,12 +3,34 @@
   <link type="text/css" href="style.css" rel="stylesheet">
 </head>
 <body>
-  <div>
 
-  </div>
-  <div>
-   <img src="logo.png">
 
+  <br><br><br>
+  <div id="parent">
+    <div id="logodiv">
+     <img src="logo.png" style='width:430px;'>
+     <br><br>
+     Level 3, 22 Mount Street, 
+     Perth, Western Australia 6000 <br><br>
+     Tel: +61 8 9263 3000 <br>
+     Email: support@realcognita.net 
+
+      <br><br>
+      <br><br>
+      Realcognita.com Pty Ltd is a West Australian based company that provides network and internet services.
+      <br><br>
+      We deliver dedicated business support for when you need it most.     
+    </div>
+    <!--
+    <div id="maintext">
+      <br><br>
+      <br><br>
+      Realcognita.com Pty Ltd is a West Australian based company that provides network and internet services.
+      <br><br>
+      We deliver dedicated business support for when you need it most.
+    </div>
+  -->
   </div>
+
 </body>
 </html>
